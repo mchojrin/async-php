41f4c8cdd4d03fb7579708c56ebf248dab9d02b0
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-01-02 20:13:24 +0100

    POC for amphp

--- amphp/index.php
+++ amphp/index.php
@@ -1,5 +1,45 @@
 <?php
 
-declare(strict_types=1);
+require_once 'vendor/autoload.php';
 
-echo "Not implemented yet".PHP_EOL;
+use Amp\Http\Client\HttpClientBuilder;
+use Amp\Http\Client\HttpException;
+use Amp\Http\Client\Request;
+use Amp\Http\Client\Response;
+use Amp\Loop;
+
+Loop::run(static function () use ($argv): \Generator {
+    $uris = [
+        "https://google.com/",
+        "https://github.com/",
+        "https://stackoverflow.com/",
+    ];
+
+    // Instantiate the HTTP client
+    $client = HttpClientBuilder::buildDefault();
+
+    $requestHandler = static function (string $uri) use ($client): \Generator {
+        /** @var Response $response */
+        $response = yield $client->request(new Request($uri));
+
+        return yield $response->getBody()->buffer();
+    };
+
+    try {
+        $promises = [];
+
+        foreach ($uris as $uri) {
+            $promises[$uri] = Amp\call($requestHandler, $uri);
+        }
+
+        $bodies = yield $promises;
+
+        foreach ($bodies as $uri => $body) {
+            print $uri . " - " . \strlen($body) . " bytes" . PHP_EOL;
+        }
+    } catch (HttpException $error) {
+        // If something goes wrong Amp will throw the exception where the promise was yielded.
+        // The HttpClient::request() method itself will never throw directly, but returns a promise.
+        echo $error;
+    }
+});
