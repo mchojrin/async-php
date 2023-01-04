b872628adf703d02db8e3584d27a085edc446d81
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2023-01-04 09:25:19 +0100

    Testing filesystem and mysql through ReactPHP

--- reactphp/index.php
+++ reactphp/index.php
@@ -2,35 +2,17 @@
 
 declare(strict_types=1);
 
+use React\Filesystem\Factory;
+use React\Filesystem\Node\FileInterface;
+
 require 'vendor/autoload.php';
 
 $before = microtime(true);
 
-$browser = new React\Http\Browser();
-$urls = [
-    "Cats" => "https://meowfacts.herokuapp.com/",
-    "Dogs" => "https://dogapi.dog/api/v2/breeds/68f47c5a-5115-47cd-9849-e45d3c378f12",
-];
+Factory::create()->detect(__FILE__)->then(function (FileInterface $file) {
+    return $file->getContents();
+})->then(static function (string $contents): void {
+    echo $contents;
+})->done();
 
-$promises = [];
-foreach ($urls as $name => $url) {
-    $promises[$name] = $browser
-        ->get($url)
-        ->then(
-            function (Psr\Http\Message\ResponseInterface $response) use ($name) {
-                echo "$name API response:" . PHP_EOL;
-                echo $response->getBody() . PHP_EOL;
-            },
-            function (Exception $e) {
-                echo 'Error: ' . $e->getMessage() . PHP_EOL;
-            });
-}
-
-try {
-    React\Async\await(React\Promise\all($promises));
-    echo "Time elapsed: ".(microtime(true) - $before).PHP_EOL;
-} catch (Throwable $e) {
-    foreach ($promises as $promise) {
-        $promise->cancel();
-    }
-}
+echo "Time elapsed: ".(microtime(true) - $before).PHP_EOL;
